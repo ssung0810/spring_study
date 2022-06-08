8fb3528b185188e9372e7a959b100b696b3a720e
ssung <qkrtjdcjf124@gmail.com>
2022-06-09 00:33:27 +0900

    feat: ãweb scope Provider적용

--- bean/src/main/java/com/example/bean/scope/web/LogDemoController.java
+++ bean/src/main/java/com/example/bean/scope/web/LogDemoController.java
@@ -1,6 +1,7 @@
 package com.example.bean.scope.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
