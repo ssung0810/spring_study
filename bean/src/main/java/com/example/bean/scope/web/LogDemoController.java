5cb0b3a3fd1db7dc21cdde5670ed56510410882e
ssung <qkrtjdcjf124@gmail.com>
2022-06-09 00:34:26 +0900

    feat: web scope proxyMode 적용

--- bean/src/main/java/com/example/bean/scope/web/LogDemoController.java
+++ bean/src/main/java/com/example/bean/scope/web/LogDemoController.java
@@ -1,7 +1,6 @@
 package com.example.bean.scope.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
-
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
